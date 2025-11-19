7597eaafcbfbcbe06639a55f1fc7c0ef39e2ca68
carlosra0098 <crg090503@gmail.com>
2025-11-19 20:56:26 +0100

    Ejercicios de hoy en carpeta Ejercicios Clase 18/11/2025

---
+++ Ejercicios Clase 18/11/2025/Ejercicio1.py
@@ -0,0 +1,19 @@
+""" Imprime un diamante hueco de altura total 2n - 1, centrado con asteriscos, donde solo se imprimen los bordes y el centro."""
+
+altura = 5
+
+for i in range(1, altura + 1):
+    espacios = altura - i
+
+    if i == 1:
+        print(" " * espacios + "*")
+    else:
+        print(" " * espacios + "*" + " " * (2 * i - 3) + "*")
+
+for i in range(altura - 1, 0, -1):
+    espacios = altura - i
+
+    if i == 1:
+        print(" " * espacios + "*")
+    else:
+        print(" " * espacios + "*" + " " * (2 * i - 3) + "*")
--- Ejercicios Parte 2/Ejercicio17p2.py
+++ Ejercicios Parte 2/Ejercicio17p2.py
@@ -1,3 +1,13 @@
 """ Programa que suma independientemente los pares y los impares de los números
 comprendidos entre 100 y 200, y luego muestra por pantalla ambas sumas."""
 
+contador_pares = 0
+contador_impares = 0
+
+for i in range(100, 200 + 1):
+    if i % 2 == 0:
+        contador_pares += 1
+    else:
+        contador_impares += 1
+        
+print("Suma pares: ", contador_pares, "Suma impares: ", contador_impares)
